3c0fec23ea5ba34779a49b8aa3994af8edb20493
冨岡　拓哉 <tomiokatakuya@tomiokatakuyanoiMac.local>
2019-07-11 10:23:56 +0900

    Add some routes

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,9 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+|############################
+| set routes of USERS bellow.
+|############################
 */
 
 Route::get('/', function () {
@@ -17,6 +12,20 @@ Route::get('/', function () {
 
 Route::get('/shoplist',  'ShopController@index');
 
+Route::get('/news', 'NewsController@index');
+
+/*
+|############################
+| set routes of ADMIN bellow.
+|############################
+*/
 Route::get('/admin', function(){
     return view('admin.top');
 });
+
+/*
+| Shop Settings
+*/
+Route::get('/admin/shop_registraion', 'ShopController@showCreateShop');
+
+Route::post('/admin/shop_registraton', 'ShopController@createShop');

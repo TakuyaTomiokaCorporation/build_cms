0282095bfbeb873c901625cf05558a4f1f98f28a
冨岡　拓哉 <tomiokatakuya@tomiokatakuyanoiMac.local>
2019-07-11 10:20:24 +0900

    Add DB connection

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -3,16 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Shop;
+use App\Region;
+use App\Prefecture;
 
 class ShopController extends Controller
 {
     public function index()
     {
-        return view('user.shoplist');
+        $shops = Shop::all();
+        return view('user.shoplist',[
+            'shops' => $shops,
+        ]);
     }
 
-    public function add(Request $request)
+    public function showCreateShop()
     {
-        return view(admin.shop.add);
+        $regions = Region::all();
+        $prefectures = Prefecture::all();
+
+        return view('admin.shop.create', [
+            'regions' => $regions,
+            'prefectures' => $prefectures,
+        ]);
+    }
+
+    public function createShop(Request $request)
+    {
+
     }
 }

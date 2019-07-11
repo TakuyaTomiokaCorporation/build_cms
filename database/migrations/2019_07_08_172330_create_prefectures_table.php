cd58570b6de3f16a38bc2845661422f487c5d95d
冨岡　拓哉 <tomiokatakuya@tomiokatakuyanoiMac.local>
2019-07-11 10:28:14 +0900

    Create table of prefectures

---
+++ database/migrations/2019_07_08_172330_create_prefectures_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePrefecturesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prefectures', function (Blueprint $table) {
+            $table->bigIncrements('prefectures_id');
+            $table->string('prefectures_name', 10);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prefectures');
+    }
+}

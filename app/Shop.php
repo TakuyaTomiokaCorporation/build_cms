013d4d9098c8be4c7db38d5b13492ef7897cf70e
冨岡　拓哉 <tomiokatakuya@tomiokatakuyanoiMac.local>
2019-07-11 16:58:11 +0900

    Protect Shop columns

--- app/Shop.php
+++ app/Shop.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Shop extends Model
 {
-    //
+    protected $table = 'shops';
+    protected $fillable = [
+        'region_id',
+        'pre_id',
+        'shop_name',
+        'shop_address',
+        'shop_tel',
+        'post_number'
+    ];
 }

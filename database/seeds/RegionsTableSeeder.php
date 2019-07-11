6a6a069b015046f9382280dd51c87fed6de2400e
冨岡　拓哉 <tomiokatakuya@tomiokatakuyanoiMac.local>
2019-07-11 18:35:44 +0900

    Add test data

--- database/seeds/RegionsTableSeeder.php
+++ database/seeds/RegionsTableSeeder.php
@@ -11,8 +11,18 @@ class RegionsTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('regions')->insert([
-            'region_name' => '北海道',
-        ]);
+        $regions = [
+            // 本番環境では北海道をコメントアウトする
+            // ['region_name' => '北海道'],
+            ['region_name' => '東北地方'],
+            ['region_name' => '関東地方'],
+            ['region_name' => '中部地方'],
+            ['region_name' => '近畿地方'],
+            ['region_name' => '中国地方'],
+            ['region_name' => '四国地方'],
+            ['region_name' => '九州地方']
+        ];
+
+        DB::table('regions')->insert($regions);
     }
 }

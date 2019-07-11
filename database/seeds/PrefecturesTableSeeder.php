36d9691b03a9637a2bd3c604310e8e71f4d0e7c8
冨岡　拓哉 <tomiokatakuya@tomiokatakuyanoiMac.local>
2019-07-11 10:28:53 +0900

    Create test data for Prefectures tables

---
+++ database/seeds/PrefecturesTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PrefecturesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('prefectures')->insert([
+            'prefectures_name' => '北海道',
+        ]);
+    }
+}

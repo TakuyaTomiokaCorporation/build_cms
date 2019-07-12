38503c5c7a4a229357381d05223774fc2e7ae107
冨岡　拓哉 <tomiokatakuya@tomiokatakuyanoiMac.local>
2019-07-12 13:36:45 +0900

    Create header for admin

--- resources/views/layouts/header_admin.blade.php
+++ resources/views/layouts/header_admin.blade.php
@@ -1,91 +1,67 @@
 <!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    {{-- CSRF トークン --}}
+    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@if (! Request::is('/')){{ $title }} | @endif{{ env('APP_NAME') }}</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-    {{-- CSS --}}
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    {{-- 個別のjavaScript読み込み --}}
+    @yield('javascript-head')
+
+    <!-- Fonts -->
+
+    <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    {{-- 個別のCSS読み込み --}}
+    @yield('css')
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    {{-- Navbarの左側 --}}
-                    <ul class="navbar-nav mr-auto">
-                        {{-- 「記事」と「ユーザー」へのリンク --}}
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ url('posts') }}">{{ __('Posts') }}</a>
+    <header>
+            <div class="containr">
+                <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                    {{-- ロゴのみ左寄せ --}}
+                    <a class="navbar-brand" href="#">会社ロゴ</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    {{-- その他のナビゲーションバーは右寄せ --}}
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">ショップ情報 <span class="sr-only">(current)</span></a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ url('users') }}">{{ __('Users') }}</a>
+                            <a class="nav-link" href="#">ニュースの投稿</a>
                         </li>
-                    </ul>
-
-                    {{-- Navbarの右側 --}}
-                    <ul class="navbar-nav ml-auto">
-                        {{-- 投稿ボタン --}}
-                        <li class="nav-item">
-                            <a href="{{ url('posts/create') }}" id="new-post" class="btn btn-success">
-                                {{ __('New Post') }}
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                特集ページの投稿
                             </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">アーティストインタビュー</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">製品レビュー動画</a>
+                            </div>
                         </li>
-
-                        {{-- 認証関連のリンク --}}
-                        @guest
-                            {{-- 「ログイン」と「ユーザー登録」へのリンク --}}
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                        @else
-                            {{-- 「プロフィール」と「ログアウト」のドロップダウンメニュー --}}
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="dropdown-user" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdown-user">
-                                    <a class="dropdown-item" href="{{ url('users/'.auth()->user()->id) }}">
-                                        {{ __('Profile') }}
-                                    </a>
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                        onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
+            </nav>
+        </header>
+    <div id="app">
+        <main class="">
+            {{-- コンテンツ部分読み込み --}}
             @yield('content')
         </main>
     </div>
 
-    {{-- JavaScript --}}
-    <script src="{{ asset('js/app.js') }}"></script>
+    {{-- 個別のjavaScript読み込み --}}
+    @yield('javascript-footer')
+
 </body>
 </html>

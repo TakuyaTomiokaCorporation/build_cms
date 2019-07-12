7b998b8ba0847b5d3f2d202dee51716cdceaf21b
冨岡　拓哉 <tomiokatakuya@tomiokatakuyanoiMac.local>
2019-07-12 13:38:19 +0900

    Create shop form

--- resources/views/admin/shop/create.blade.php
+++ resources/views/admin/shop/create.blade.php
@@ -1,21 +1,66 @@
-<body>
-    <p>ショップの作成</p>
-    <form action="" method="post">
-        @csrf
-        <div class="form-group">
-            <label for="region">地域</label>
-            @foreach ($regions as $region)
-                <select name="region" id="" size="1">
-                    <option value="{{ $region -> region_id }}">{{ $region -> region_name}}</option>
-                </select>
-            @endforeach
-            <label for="prefecture">都道府県</label>
-            @foreach ($prefectures as $prefecture)
-                <select name="prefecture" id="" size="1">
-                    <option value="{{ $prefecture -> prefectures_id }}">{{ $prefecture -> prefectures_name}}</option>
-                </select>
-            @endforeach
-        </div>
-    </form>
-</body>
-</html>
+@extends('layouts.header_admin')
+
+@section('content')
+    <h1 class="text-center jumbotron">ショップの作成</h1>
+    <div class="container">
+        @if ($errors->any())
+            <div class="alert alert-danger dismissible fade show" role="alert">
+                @foreach ($erros as $error)
+                    <strong>注意!</strong> {{ $error}}
+                    <button type="button" class="close" data-dismiss="alert" aria-label="閉じる">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                @endforeach
+            </div>
+        @endif
+    </div>
+    <div class="container">
+        <form class="form-horizontal" action="{{ route('shop.confirm')}}" method="post">
+            @csrf
+            @method('POST')
+            <div class="form-group row">
+                <label class="control-label col-sm-2" for="region">地域</label>
+                <input type="text" name="region" class="col-sm-10 form-control-lg" value="{{ old('region') }}" required>
+                @if($errors->has('region'))<br><span class="error">{{ $errors->first('region') }}</span> @endif
+                {{-- 下記は今後使用予定
+                <select name="region" id="" size="1" class="custom-select col-sm-10">   
+                        @foreach ($regions as $region)
+                        <option value="{{ $region -> region_id }}">{{ $region -> region_name}}</option>
+                    @endforeach 
+                </select> --}}
+            </div>
+            <div class="form-group row">
+                <label class="control-label col-sm-2" for="prefecture">都道府県</label>
+                <input type="text" name="prefecture" class="col-sm-10 form-control-lg" value="{{ old('prefecture') }}" required>
+                @if($errors->has('prefecture'))<br><span class="error">{{ $errors->first('prefecture') }}</span> @endif
+                {{--下記は今後使用予定
+                <select name="prefecture" id="" size="1" class="custom-select col-sm-10"> 
+                        @foreach ($prefectures as $prefecture)
+                        <option value="{{ $prefecture -> prefectures_id }}">{{ $prefecture -> prefectures_name}}</option>
+                    @endforeach 
+                </select>--}}
+            </div>
+            <div class="form-group row">
+                <label class="control-label col-sm-2" for="shop_name">店舗名</label>
+                <input class="col-sm-10 form-control-lg" name="shop_name" type="text" id="shop_name" name="shop_name" value="{{ old('shop_name') }}" required>
+                @if($errors->has('shop_name'))<br><span class="error">{{ $errors->first('shop_name') }}</span> @endif
+            </div>
+            <div class="form-group row">
+                <label class="control-label col-sm-2" for="post_number">郵便番号</label>  
+                <input class="col-sm-10 form-control-lg" name="post_number" type="text" id="post_number" name="post_number" value="{{ old('post_number') }}" required>
+                @if($errors->has('post_number'))<br><span class="error">{{ $errors->first('post_number') }}</span> @endif
+            </div>
+            <div class="form-group row">
+                <label class="control-label col-sm-2" for="shop_address">住所</label>
+                <input class="col-sm-10 form-control-lg" name="shop_address" type="text" id="shop_address" name="shop_address" value="{{ old('shop_address') }}" required>
+                @if($errors->has('shop_address'))<br><span class="error">{{ $errors->first('shop_address') }}</span> @endif 
+            </div>
+            <div class="form-group row">
+                <label class="control-label col-sm-2" for="shop_tel">電話番号</label>
+                <input class="col-sm-10 form-control-lg" name="shop_tel" type="text" id="shop_tel" name="shop_tel" value="{{ old('shop_tel') }}" required>
+                @if($errors->has('shop_tel'))<br><span class="error">{{ $errors->first('shop_tel') }}</span> @endif
+            </div>
+            <button type="submit" name="submit" class="btn btn-primary">{{ __('Submit') }}</button>
+        </form>
+    </div>
+@endsection

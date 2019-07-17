771505b0e51672ca77ccafb16e1bb8cb51d20782
冨岡　拓哉 <tomiokatakuya@tomiokatakuyanoiMac.local>
2019-07-17 13:29:54 +0900

    change name finish to store

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,7 @@ Route::group(['prefix' => '/admin/shop'], function(){
     Route::get('/', 'ShopController@getIndex')->name('shop');
     Route::get('create', 'ShopController@create')->name('shop.create');
     Route::patch('confirm', 'ShopController@confirm')->name('shop.confirm');
-    Route::post('finish', 'ShopController@finish')->name('shop.finish');
+    Route::post('finish', 'ShopController@store')->name('shop.store');
 });
 
 // Route::group(['middleware' => ['web']], function () {
@@ -46,5 +46,5 @@ Route::group(['prefix' => '/admin/product','middlwware'=>'web'], function(){
     Route::get('/', 'ProductController@getList')->name('product');
     Route::get('create', 'ProductController@create')->name('product.create');
     Route::patch('confirm', 'ProductController@confirm')->name('product.confirm');
-    Route::post('finish', 'ProductController@finish')->name('product.finish');
+    // Route::post('finish', 'ProductController@finish')->name('product.store');
 });

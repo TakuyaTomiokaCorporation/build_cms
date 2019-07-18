1f53a5795bb409789936d258666aaca7dc935e09
冨岡　拓哉 <tomiokatakuya@tomiokatakuyanoiMac.local>
2019-07-18 13:14:00 +0900

    Edit authorize true to false and Add rules of image

--- app/Http/Requests/ValiProductRequest.php
+++ app/Http/Requests/ValiProductRequest.php
@@ -13,7 +13,7 @@ class ValiProductRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,7 @@ class ValiProductRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'thumbnail' => 'required|file|image|mimes:jpeg,png,jpg,gif|max:2048'
         ];
     }
 }

818fd024183b26aac1cf997904c8ec711f0e092b
冨岡　拓哉 <tomiokatakuya@tomiokatakuyanoiMac.local>
2019-07-18 13:17:58 +0900

    Create product_news

---
+++ resources/views/admin/product_news/create.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.header_admin')
+
+@section('content')
+    <div class="container">
+        <h1 class="text-center jumbotron">製品に関するお知らせの作成</h1>
+    </div>
---
+++ resources/views/admin/product_news/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.header_admin')
+
+@section('content')
+    <div class="container">
+        <h1 class="text-center jumbotron">製品に関するお知らせのトップページ</h1>
+    </div>

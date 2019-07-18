c36ef1642b2d8e32b021fefcfe9bda38138d8855
冨岡　拓哉 <tomiokatakuya@tomiokatakuyanoiMac.local>
2019-07-18 18:05:03 +0900

    Edit name main_visual -> image and Add name attribute both of date and time

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -41,7 +41,7 @@
             <hr>
             <p>画像</p>
             <div class="">
-                <input type="file" name="main_visual"> 
+                <input type="file" name="image"> 
             </div>
             <hr>
             <p>PDF</p>
@@ -53,11 +53,11 @@
             <div class="row">
                 <div class="form-group">
                     <label for="date">日付</label>
-                    <input id="date" type="date" min="{{ $today }}">
+                    <input id="date" type="date" min="{{ $today }}" name="date">
                 </div>
                 <div class="form-group">
                     <label for="time">時間</label>
-                    <input id="time" type="time" min="{{ $time }}">
+                    <input id="time" type="time" name="time">
                 </div>
             </div>
             <hr>

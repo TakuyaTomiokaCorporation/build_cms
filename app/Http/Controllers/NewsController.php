4a6afa1cb941ef36d390afe02168ebdf96b84a93
冨岡　拓哉 <tomiokatakuya@tomiokatakuyanoiMac.local>
2019-07-18 18:08:53 +0900

    Edit @confirm to join date and time

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\News;
+use Carbon\Carbon;
+
 class NewsController extends Controller
 {
     /************************************************
@@ -38,18 +40,35 @@ class NewsController extends Controller
 
     public function create(){
 
-        return view('admin.news.create');
+        $today = Carbon::now()->format('Y-m-d');
+        
+
+        return view('admin.news.create',[
+            'today' => $today,
+        ]);
     }
 
     public function confirm(Request $request){
 
         $news_confirm = $request -> all();
 
+        // Join date and time for creating book_date
+        $news_confirm['book_date'] = $request->date .' '.$request->time;
+
+
         if($file = $request->file('image')){
             $name = uniqid() . $file -> getClientOriginalName();
             $file -> move('images/news/', $name);
             $news_confirm['image'] = $name;
             };
+        
+        if($file = $request->file('pdf')){
+            $name = uniqid() . $file -> getClientOriginalName();
+            $file -> move('images/product_news/', $name);
+            $product_news_confirm['pdf'] = $name;
+            };
+
+
         return view('admin.news.confirm')->with($news_confirm);
     }
 

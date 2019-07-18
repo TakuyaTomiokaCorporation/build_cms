dfb672d0172f2a85196663e3ce7b12308a13641c
冨岡　拓哉 <tomiokatakuya@tomiokatakuyanoiMac.local>
2019-07-18 18:10:28 +0900

    Create News model -> news table

---
+++ app/News.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class News extends Model
+{
+    protected $table = 'news';
+
+    protected $fillable = [
+        'title',
+        'content',
+        'category',
+        'image',
+        'pdf',
+        'release',
+        'book_date'
+    ];
+
+}

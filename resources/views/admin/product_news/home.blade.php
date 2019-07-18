01ea6ce307ecd71092eaefed3002206154983dc0
冨岡　拓哉 <tomiokatakuya@tomiokatakuyanoiMac.local>
2019-07-18 15:12:00 +0900

    Delete content and Add edit bottom

--- resources/views/admin/product_news/home.blade.php
+++ resources/views/admin/product_news/home.blade.php
@@ -1,15 +1,17 @@
 @extends('layouts.header_admin')
 
 @section('content')
-    <div class="container">
+    <div class="container mt-5">
         <h1 class="text-center jumbotron">製品に関するお知らせのトップページ</h1>
     </div>
     <div class="container">
+        <a href="{{ route('product_news.create')}}" class="btn btn-primary">新規作成</a>
+    </div>  
+    <div class="container mt-5">
         <table class="table table-striped table-hover">
             <thead>
                 <tr>
                     <th>タイトル</th>
-                    <th>内容</th>
                     <th>画像</th>
                     <th>PDF</th>
                     <th>公開設定</th>
@@ -20,9 +22,8 @@
                 @foreach($news_products as $news_product)
                 <tr>
                     <td>{{ $news_product->title }}</td>
-                    <td>{{ $news_product->content }}</td>
-                    <td><img src="/images/thumbnails/{{ $news_product->main_visual }}" alt=""></td>
-                    <td>{{ $product->overview }}</td>
+                    <td><img src="/images/product_news/{{ $news_product->main_visual }}" alt=""></td>
+                    <td>{{ $news_product->pdf }}</td>
                     <td>
                         @if($news_product->release)
                         公開中
@@ -32,6 +33,7 @@
                     </td>
                     <td>
                     <a href="" class="btn btn-primary btn-sm">詳細</a>
+                    <a href="" class="btn btn-primary btn-sm">編集</a>
                     <a href="" class="btn btn-danger btn-sm">削除</a>
                     </td>
                     </tr>

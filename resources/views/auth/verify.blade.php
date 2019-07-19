9c3f3c60f8a466b33757a7512aaaf0e746065236
冨岡　拓哉 <tomiokatakuya@tomiokatakuyanoiMac.local>
2019-07-19 15:49:25 +0900

    Edit language to ja

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -5,17 +5,16 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card-header">{{ __('メールアドレスが正しいか確認する') }}</div>
 
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            {{ __('新しい確認リンクがあなたのメールアドレスに送信されました') }}
                         </div>
                     @endif
-
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }}, <a href="{{ route('verification.resend') }}">{{ __('click here to request another') }}</a>.
+                    {{ __('手続きを進める前に、確認リンクがあるかどうかメールボックスを確認してください') }}
+                    {{ __('もしメールを受け取っていない場合は、') }}, <a href="{{ route('verification.resend') }}">{{ __('こちらをクリックして再度送信してください。') }}</a>.
                 </div>
             </div>
         </div>

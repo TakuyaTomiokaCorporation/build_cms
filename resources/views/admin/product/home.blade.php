31551514833db76e9fe20ef376fe46aaf2c7b39f
冨岡　拓哉 <tomiokatakuya@tomiokatakuyanoiMac.local>
2019-07-22 16:12:38 +0900

    Add route of edit

--- resources/views/admin/product/home.blade.php
+++ resources/views/admin/product/home.blade.php
@@ -25,6 +25,7 @@
                     <td>{{ $product->overview }}</td>
                     <td>
                     <a href="" class="btn btn-primary btn-sm">詳細</a>
+                    <a href="{{ route('product.edit', $product->id) }}" class="btn btn-primary btn-sm">編集</a>
                     <a href="" class="btn btn-danger btn-sm">削除</a>
                     </td>
                     </tr>

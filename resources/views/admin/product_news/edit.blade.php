dbcea5f5d3c75c35a04101d12193cf4f4ce8aef0
冨岡　拓哉 <tomiokatakuya@tomiokatakuyanoiMac.local>
2019-07-22 16:34:02 +0900

    Create edit page

---
+++ resources/views/admin/product_news/edit.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.admin')
+
+@section('main')
+    <div class="container mt-5">
+        <h1 class="text-center jumbotron">製品に関するお知らせの編集</h1>
+    </div>
+    <div class="container">
+        <a href="{{ route('home')}}" class="btn btn-outline-primary">トップ</a>
+        <a href="{{ route('product_news')}}" class="btn btn-outline-secondary ml-3">戻る</a>
+    </div>
+    <div class="container mt-5">
+        <form  class="form-horizontal" action="{{ route('product_news.update', $news_product->id) }}" method="POST" enctype='multipart/form-data'>{{-- enctype="multipart/form-data" --}}
+            @csrf
+            @method('PATCH')
+            <div class="form-group row">
+                <label class="control-label col-sm-2" for="title">タイトル</label>
+                <input class="col-sm-10 form-control-lg" name="title" type="text" id="title" value="{{ $news_product->title }}">
+            </div>
+            <div class="form-group row">
+                <label class="control-label col-sm-2" for="content">内容</label>
+                <textarea class="form-control" name="content" id="content" rows="5" placeholder="お知らせの内容を入力してください。">{{ $news_product->content }}</textarea>
+            </div>
+            <hr>
+            <p>画像</p>
+            <div class="">
+                <input type="file" name="main_visual"> 
+            </div>
+            <hr>
+            <p>PDF</p>
+            <div class="">
+                <input type="file" name="pdf"> 
+            </div>
+            <hr>
+            <p>公開設定</p>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="release" id="release" value="1">
+                <label class="form-check-label" for="release">公開</label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="release" id="close" value="0">
+                <label class="form-check-label" for="close">非公開</label>    
+            </div>
+            <hr>
+            <button type="submit" class="btn btn-primary">更新</button>
+        </form>
+    </div>
+@endsection

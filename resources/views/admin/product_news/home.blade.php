63908e57d476643e8a513843804fc28031d554a2
冨岡　拓哉 <tomiokatakuya@tomiokatakuyanoiMac.local>
2019-07-22 16:36:24 +0900

    Add route of edit

--- resources/views/admin/product_news/home.blade.php
+++ resources/views/admin/product_news/home.blade.php
@@ -33,7 +33,7 @@
                     </td>
                     <td>
                     <a href="" class="btn btn-primary btn-sm">詳細</a>
-                    <a href="" class="btn btn-primary btn-sm">編集</a>
+                    <a href="{{ route('product_news.edit', $news_product->id) }}" class="btn btn-primary btn-sm">編集</a>
                     <a href="" class="btn btn-danger btn-sm">削除</a>
                     </td>
                     </tr>

5d846c61ff7a481e4fbcc61af374a7880a9e6d3c
冨岡　拓哉 <tomiokatakuya@tomiokatakuyanoiMac.local>
2019-07-22 14:07:15 +0900

    Edit top page layout

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -1,7 +1,24 @@
 @extends('layouts.header_user')
 
 @section('content')
-    <div class="jumbotron">
-        <h1 class="text-center">ホーム画面</h1>
+    <div class="container">
+        <img src="images/user/main_visual.png" class="max-auto d-block" style="width:100%" alt="画像">
     </div>
+    <div class="container mt-5">
+        <h3>最新情報</h3>
+        <div class="container">
+            <div class="row">
+                @foreach ($posts as $post)
+                <div class="col-xs-2">
+                    <a href="{{ route('news.show', $post->id) }}">
+                    <img src="images/news/{{ $post->image }}" alt="">
+                    <span>{{ $post->category }}</span>
+                    <h4>{{ $post->title }}</h4>
+                    </a>
+                </div>
+                @endforeach
+            </div>       
+        </div>
+    </div>
+
 @endsection

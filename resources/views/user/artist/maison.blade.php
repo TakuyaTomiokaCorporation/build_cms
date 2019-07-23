758554c273b0399e9b1cfb20001f4a2d886c0e4f
冨岡　拓哉 <tomiokatakuya@tomiokatakuyanoiMac.local>
2019-07-23 18:17:09 +0900

    Create maison page

--- resources/views/user/artist/maison.blade.php
+++ resources/views/user/artist/maison.blade.php
@@ -1,16 +1,54 @@
 @extends('layouts.user_master')
 
-@section('title', ' |')
+@section('title', 'Maison book girl×TE-D01d |')
 
-@section('description', '')
+@section('description', 'アーティストによるインタビュー動画、AVIOT製品のレビュー、操作方法')
 
-@section('body_id', '')
+@section('body_id', 'maisonbookgirl')
 
 @section('css')
     
 @endsection
 
 @section('content')
+
+<div id="container" class="both mb60">
+<div id="" class="main" role="main">
+    <article class="w1020 w98 m_auto">
+        <h2 class="pt20 border_b mb20">Maison book girl×TE-D01d</h2>
+        <section class="artist_movie">
+            <div><img src="{{ asset('images/artists/maisonbookgirl_mv.jpg') }}" alt="Maison book girl"></div>
+            <div>
+                <h3 class="mt20">Maison book girl</h3>
+                <p class="mt10">矢川葵、井上唯、和田輪、コショージメグミ、による4人組グループ。通称「ブクガ」。 音楽家サクライケンタが楽曲から世界観の構築まで、トータルでプロデュースを行う。その独特且つ前衛的な音楽性、他には類を見ない世界観が話題を呼び、2016年11月メジャーデビュー。</p>
+                <p class="mt10">その1年後、2017年末にはZepp DiverCityにてワンマンライブを開催(※「Solitude HOTEL 4F」)するほどに。 2018年5月にはポニーキャニオンへの移籍を発表。</p>
+                <p class="mt10">そこからは立て続けに、「ビバラポップ」への出演、イギリス・ブライトンでの国際音楽フェス「THE GREAT ESCAPE FESTIVAL2018」に日本代表として出演、EP「elude」のリリース(※収録曲「レインコートと首の無い鳥」がピッコマのTV CMタイアップ)、「Solitude HOTEL 5F」(@日本青年会ホール)開催、と一気に活動のフィールドを大きくしていった。</p>
+                <p class="mt10">11月には2ndフルアルバム「yume」を発売。Interludeを含む全21曲で構成。禍々しくもありながら美しさを醸す作品は業界内外の音楽マニアの間で絶賛され、高い評価を受けた。このタイミングにて、NHK「シブヤノオト」で初のTV歌ポーズを披露したことで大きな話題に。</p>
+                <p class="mt10">また11月末から12月にかけて、「Solitude HOTEL 6F hiru / yoru / yume」を開催(hiru / yoru @日本橋三井ホール・yume@ヒューリックホール東京)。全く内容の違う公演を3回行う異例のワンマンライブも各所で絶賛された。</p>
+                <p class="mt10">2019年は3月より福岡・大阪・名古屋を回るツアーの後、シングル「SOUP」をリリース。そして自身最大規模となる「Solitude HOTEL 7F」を4月14日に昭和女子大学・人見記念講堂で開催。6月からは自身初のライブハウスツアー「Maison book girl LIVE HOUSE TOUR 2019」をスタートさせる。</p>
+            </div>
+            <dl class="mt20">
+                <dt>■Maison book girl オフィシャルHP</dt>
+                <dd><a href="http://www.maisonbookgirl.com/" target="_blank">http://www.maisonbookgirl.com/</a></dd>
+    
+                <dt class="mt10">■Maison book girl Twitter</dt>
+                <dd><a href="https://twitter.com/maisonbookgirl/" target="_blank">Twitter https://twitter.com/maisonbookgirl/</a></dd>
+    
+                <dt class="mt10">■Maison book girl Instagram</dt>
+                <dd><a href="https://www.instagram.com/maison_book_girl/" target="_blank">https://www.instagram.com/maison_book_girl/</a></dd>
+            </dl>
+    
+            <div class="mt20 alc">
+                <iframe width="600" height="400" src="https://www.youtube.com/embed/lPjfUp1MuYg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+    
+            <div class="alc mt20">
+                <a href="../../product/TE-D01d/" target="_blank" class="btn_back mt10">製品詳細はこちら</a>
+            </div>  
+        </section>     
+    </article>
+</div><!--/main-->
+</div><!--/container-->
     
 @endsection
 

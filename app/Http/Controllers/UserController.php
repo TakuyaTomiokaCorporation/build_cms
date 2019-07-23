935febe9731d03a96ad022d0730bc624213f48e0
冨岡　拓哉 <tomiokatakuya@tomiokatakuyanoiMac.local>
2019-07-23 15:47:07 +0900

    Add controller and model

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,20 +3,58 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+
+// relatedo to News Model
 use App\News;
+
+// related to Shop Model
+use App\Shop;
+use App\Region;
+use App\Prefecture;
+
+//related to ProductNews Model
+use App\ProductNews;
+
 
 class UserController extends Controller
 {
     public function getAllInfo(){
-        $posts = \DB::table('news')->latest()->get();
 
-        return view('user.home', compact('posts'));
+        $posts = \DB::table('news')->latest()->get();
+        $news_products = \DB::table('news_products')->latest()->get();
+
+        return view('user.home', compact('posts', 'news_products'));
     }
 
-    public function show($id){
+    public function getNewsInfo($id)
+    {
         $news = News::findOrFail($id);
         // dd($news);
         return view('user.news', compact('news'));
+    }
+
+    public function getShopInfo()
+    {
+        $shops = Shop::all();
+        return view('user.shoplist',[
+            'shops' => $shops,
+        ]);
+    }
+
+    public function getProductNewsInfo()
+    {
+        $news_products = \DB::table('news_products')->latest()->take(5);
+
+    }
+
+    public function getAbout()
+    {
+        return view('user.about');
+    }
+
+    public function getProductInfo()
+    {
+
     }
 
 }

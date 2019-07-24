6bb3039aa7c5786a680f882ef525807d43e55300
冨岡　拓哉 <tomiokatakuya@tomiokatakuyanoiMac.local>
2019-07-24 15:07:17 +0900

    Create registration route

--- routes/web.php
+++ routes/web.php
@@ -68,6 +68,7 @@ Route::get('/product/WE-D01c', 'UserController@getWed01c');
 */
 
 Route::get('/support', 'UserController@getSupport')->name('support');
+Route::get('/support/registration', 'UserController@getProductRegistration')->name('product_regiser');
 
 
 

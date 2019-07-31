95083d01cb0ba819706625df3adb752e30222cda
冨岡　拓哉 <tomiokatakuya@tomiokatakuyanoiMac.local>
2019-07-31 12:17:31 +0900

    Edit brand image

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -23,8 +23,8 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    aviot
+                <a class="navbar-brand" href="{{ route('home') }}">
+                    <img src="{{ asset('images/user/logo_w.png') }}" alt="AVIOT">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>

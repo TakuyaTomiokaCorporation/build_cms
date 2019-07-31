d5c360d018bde1199fa8072c80c5c79867e8dba0
冨岡　拓哉 <tomiokatakuya@tomiokatakuyanoiMac.local>
2019-07-31 11:14:49 +0900

    Edit product image page card

--- resources/views/admin/top.blade.php
+++ resources/views/admin/top.blade.php
@@ -7,11 +7,11 @@
         {{-- 製品登録 --}}
         <div class="col-sm-6">
             <div class="card img-thumbnail">
-                <img class="card-img-top" src="/images/admin/product.jpg" alt="製品登録の画像">
+                <img class="card-img-top" src="{{ asset('images/admin/product.jpeg') }}" alt="製品登録の画像">
                 <div class="card-body px-2 py-3">
                 <h5 class="card-title">製品登録</h5>
                 <p class="card-text">製品の登録や編集はこちらから行えます。</p>
-                <p class="mb-0"><a href={{ route('product')}} class="btn btn-primary stretched-link">ボタン</a></p>
+                <p class="mb-0"><a href={{ route('product')}} class="btn btn-primary stretched-link">表示</a></p>
                 </div>
             </div>
         </div>
@@ -22,7 +22,7 @@
                 <div class="card-body px-2 py-3">
                 <h5 class="card-title">最新情報</h5>
                 <p class="card-text">最新情報の登録はこちらから行えます。</p>
-                <p class="mb-0"><a href="{{ route('news') }}" class="btn btn-primary stretched-link">ボタン</a></p>
+                <p class="mb-0"><a href="{{ route('news') }}" class="btn btn-primary stretched-link">表示</a></p>
                 </div>
             </div>
         </div>
@@ -37,7 +37,7 @@
                         <div class="card-body px-2 py-3">
                         <h5 class="card-title">製品に関するお知らせ</h5>
                         <p class="card-text">製品に関するお知らせはこちらから行えます。</p>
-                        <p class="mb-0"><a href="{{ route('product_news')}}" class="btn btn-primary stretched-link">ボタン</a></p>
+                        <p class="mb-0"><a href="{{ route('product_news')}}" class="btn btn-primary stretched-link">表示</a></p>
                         </div>
                     </div>
                 </div>
@@ -48,7 +48,7 @@
                         <div class="card-body px-2 py-3">
                         <h5 class="card-title">店舗情報の登録</h5>
                         <p class="card-text">店舗情報の登録や編集はこちらから行えます。</p>
-                        <p class="mb-0"><a href="{{ route('shop')}}" class="btn btn-primary stretched-link">ボタン</a></p>
+                        <p class="mb-0"><a href="{{ route('shop')}}" class="btn btn-primary stretched-link">表示</a></p>
                         </div>
                     </div>
                 </div>

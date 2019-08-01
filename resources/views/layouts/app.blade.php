e4bb558c79f67d7c6d5d9f1bfb1b7bc364ca7d10
冨岡　拓哉 <tomiokatakuya@tomiokatakuyanoiMac.local>
2019-08-01 15:23:00 +0900

    Add google recaptcha

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,14 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- Google ReCapcha -->
+    <script src="https://www.google.com/recaptcha/api.js"></script>
+    <script>
+        function timestamp() 
+        { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500); 
+    </script>
+    
 </head>
 <body>
     <div id="app">

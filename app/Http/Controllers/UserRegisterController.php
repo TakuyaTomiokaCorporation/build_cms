1ee1ae92b645993dfd1c76df53f947f4cc3cc3c2
冨岡　拓哉 <tomiokatakuya@tomiokatakuyanoiMac.local>
2019-08-05 10:58:09 +0900

    Crate product registration form's controller

--- app/Http/Controllers/UserRegisterController.php
+++ app/Http/Controllers/UserRegisterController.php
@@ -3,8 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\UserRegister;
 
 class UserRegisterController extends Controller
 {
-    //
+    public function getSupport()
+    {
+        return view('user.support');
+    }
+
+    public function getProductRegistration()
+    {
+        return view('user.support.registration');
+    }
+
+    public function getConfirm(UserRegister $request)
+    {
+        $validatedData = $request;
+        // dd($validatedData);
+        return view('user.support.confirm', compact('validatedData'));
+    }
+
+    public function getProductThanks()
+    {
+        return view('user.support.thanks');
+    }
+
 }

423764fb718c8cb461aa1be1368af7bfad512383
冨岡　拓哉 <tomiokatakuya@tomiokatakuyanoiMac.local>
2019-08-05 11:00:16 +0900

    Edit form view

--- resources/views/user/support/registration.blade.php
+++ resources/views/user/support/registration.blade.php
@@ -18,21 +18,27 @@
         <p class="read">弊社商品の保証登録は下記項目をご入力の上、送信をお願い致します。必要事項をご記入の上、送信ボタンを押してください。</p>
         <div class="registration_form mt40">
             <p>※<span class="required">必須</span>は入力必須項目となります。</p>
-            <form accept="confirmation" method="POST">
+                <form action="https://webto.salesforce.com/servlet/servlet.WebToLead?encoding=UTF-8" method="POST">
+                <input type=hidden name='captcha_settings' value='{"keyname":"aviotcap","fallback":"true","orgId":"00D6F000002Kz08","ts":""}'>
+                <input type=hidden name="oid" value="00D6F000002Kz08">
+                <input type=hidden name="retURL" value="{{ route('product_register.thanks') }}">
+                <input type="hidden" name="00N6F00000WyN80" value="A" />
+                @csrf
+                @method('POST')
                 <dl>
                     <dt><span class="required">必須</span>お名前</dt>
-                    <dd><input  id="last_name" maxlength="80" name="last_name" size="20" type="text" placeholder="姓"  required="true"/>
-                        <input  id="first_name" maxlength="40" name="first_name" size="20" type="text"  placeholder="名"  required="true"/>
+                    <dd><input  id="last_name" maxlength="40" name="last_name" size="20" type="text" placeholder="姓"  required value="{{ old('last_name') }}">
+                        <input  id="first_name" maxlength="40" name="first_name" size="20" type="text"  placeholder="名"  required value="{{ old('first_name') }}">
                     </dd>
     
                     <dt><span class="required">必須</span>フリガナ</dt>
-                    <dd><input  id="00N6F00000SUcr2" maxlength="255" name="00N6F00000SUcr2" size="20" type="text" placeholder="セイ"  required="true"/>
-                        <input  id="00N6F00000SUcr7" maxlength="255" name="00N6F00000SUcr7" size="20" type="text" placeholder="メイ"  required="true"/>
+                    <dd><input  id="00N6F00000SUcr2" maxlength="255" name="00N6F00000SUcr2" size="20" type="text" placeholder="セイ"  required value="{{ old('00N6F00000SUcr2') }}">
+                        <input  id="00N6F00000SUcr7" maxlength="255" name="00N6F00000SUcr7" size="20" type="text" placeholder="メイ"  required value="{{ old('00N6F00000SUcr7') }}">
                     </dd>
     
                     <dt><span class="required">必須</span>性別</dt>
                     <dd>
-                        <select  id="00N6F00000MmEeS" name="00N6F00000MmEeS" title="性別"  required="true" />
+                        <select  id="00N6F00000MmEeS" name="00N6F00000MmEeS" title="性別"  required>
                             <option value="">--なし--</option>
                             <option value="男">男</option>
                             <option value="女">女</option>
@@ -40,55 +46,66 @@
                     </dd>
     
                     <dt><span class="required">必須</span><label for="zip">郵便番号</label></dt>
-                    <dd><input  id="zip" maxlength="10" name="zip" size="20" type="text" required="true" required="true" placeholder="1234567"/></dd>
+                    <dd><input  id="zip" maxlength="10" name="zip" size="20" type="text" required placeholder="1234567"/></dd>
     
                     <dt><span class="required">必須</span><label for="state">都道府県</label></dt>
-                    <dd><input  id="state" maxlength="20" name="state" size="20" type="text" required="true" placeholder="東京都"/></dd>
+                    <dd><input  id="state" maxlength="20" name="state" size="20" type="text" required placeholder="東京都"/></dd>
     
                     <dt><span class="required">必須</span><label for="city">市区郡</label></dt>
-                    <dd><input  id="city" maxlength="40" name="city" size="20" type="text" required="true" placeholder="豊島区"/></dd>
+                    <dd><input  id="city" maxlength="40" name="city" size="20" type="text" required placeholder="豊島区"/></dd>
     
                     <dt><span class="required">必須</span><label for="city">町名・番地</label></dt>
                     <dd><input type="text" name="N6F00000Mmgax" value="" size="40" ><dd>
     
                     <dt><label for="phone">電話番号</label></dt>
-                    <dd><input  id="phone" maxlength="40" name="phone" size="20" type="text" placeholder="090-0000-0000" pattern="\d{8,}" title="正式な電話番号をご入力ください。" /></dd>
+                    <dd><input  id="phone" maxlength="40" name="phone" size="20" type="text" placeholder="090-0000-0000" pattern="^\d{2,4}[-]\d{2,4}[-]\d{2,4}$" title="正式な電話番号をご入力ください。" /></dd>
     
                     <dt><span class="required">必須</span><label for="email">メールアドレス</label></dt>
-                    <dd><input  id="email" maxlength="80" name="email" size="60" type="text"  required="true" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" title="メールアドレスをご確認ください。"　required="true"/></dd>
+                    <dd><input  id="email" maxlength="80" name="email" size="60" type="text"  required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" title="メールアドレスをご確認ください。"　required/></dd>
     
-                    <dt><span class="required">必須</span><label for="email">機種名</label></dt>
+                    <dt><span class="required">必須</span><label for="00N6F00000O5eOx">機種名</label></dt>
                     <dd>
-                    <select  id="00N6F00000O5eOx" name="00N6F00000O5eOx" title="機種名" required="true">
+                    <select  id="00N6F00000O5eOx" name="00N6F00000O5eOx" title="機種名" required>
                         <option value="">--なし--</option>
-                        <option value="">TE-BD21f</option>
-                        <option value="">TE-BD21f-pnk</option>
-                        <option value="">WE-BD21d</option>
-                        <option value="">TE-D01g</option>
-                        <option value="">TE-D01d</option>
-                        <option value="">TE-D01a</option>
-                        <option value="">TE-D01b</option>
-                        <option value="">TE-D01c</option>
-                        <option value="">TE-D01d-kzn</option>
-                        <option value="">TE-D01e</option>
-                        <option value="">TE-D01h</option>
-                        <option value="">WE-D01c</option>
-                        <option value="">WE-D01b</option>
+                        <option value="TE-BD21f">TE-BD21f</option>
+                        <option value="TE-BD21f-pnk">TE-BD21f-pnk</option>
+                        <option value="WE-BD21d">WE-BD21d</option>
+                        <option value="TE-D01g">TE-D01g</option>
+                        <option value="TE-D01d">TE-D01d</option>
+                        <option value="TE-D01a">TE-D01a</option>
+                        <option value="TE-D01b">TE-D01b</option>
+                        <option value="TE-D01c">TE-D01c</option>
+                        <option value="TE-D01d-kzn">TE-D01d-kzn</option>
+                        <option value="TE-D01e">TE-D01e</option>
+                        <option value="TE-D01h">TE-D01h</option>
+                        <option value="WE-D01c">WE-D01c</option>
+                        <option value="WE-D01b">WE-D01b</option>
+                    </select>
+                    </dd>
+                    
+    
+                    <dt>色</dt>
+                    <dd>
+                    <select  id="00N6F00000MmEeh" name="00N6F00000MmEeh" title="カラー" required>
+                        <option value="">--なし--</option>
+                        <option value="ネイビー">ネイビー</option>
+                        <option value="ピンク">ピンク</option>
+                        <option value="シルバー">シルバー</option>
+                        <option value="ブラック">ブラック</option>
+                        <option value="ホワイト">ホワイト</option>
+                        <option value="レッド">レッド</option>
                     </select>
                     </dd>
     
-                    <dt>色</dt>
-                    <dd><input  id="00N6F00000MmEeh" maxlength="255" name="00N6F00000MmEeh" size="20" type="text" /></dd>
-    
                     <dt><span class="required">必須</span>シリアルナンバー</dt>
-                    <dd><input  id="00N6F00000MmEeh" maxlength="255" name="00N6F00000MmEeh" size="20" type="text" required="true"/></dd>
+                    <dd><input  id="00N6F00000MmEem" maxlength="255" name="00N6F00000MmEem" size="20" type="text" required pattern="^[WT][A-Z]{2,4}\d{7,8}$"/></dd>
     
                     <dt>購入日</dt>
                     <dd><input type="date" value="2019-01-01" min="2015-01-01" id="00N6F00000MmEer" name="00N6F00000MmEer"></dd>
     
                     <dt><label for="email">購入店（miraporta）</label></dt>
                     <dd>
-                    <select  id="00N6F00000MmEew" name="00N6F00000MmEew" required="true">
+                    <select  id="00N6F00000MmEew" name="00N6F00000MmEew">
                         <option value="">--なし--</option>
                         <option value="楽天">楽天</option>
                         <option value="yahoo">yahoo</option>
@@ -106,8 +123,9 @@
                         <input type="checkbox" id="box-2">
                         <label for="box-2">【プライバシーポリシーに同意する】</label>
                         <p>※こちらの『<a href="https://v-trade.co.jp/company/#cap1" target="_blank">PRIVACY POLICY</a>』の内容をご確認の上ご同意ください。</p>
-                        <p>※製品登録可能な正規販売店についての詳細は、『<a href="../../shop/" target="_blank">こちら</a>』をご覧ください。</p>
+                        <p>※製品登録可能な正規販売店についての詳細は、『<a href="{{ route('shoplist') }}" target="_blank">こちら</a>』をご覧ください。</p>
                     </div>
+                    {{-- <div class="g-recaptcha" data-sitekey="6Ldnm68UAAAAAIHuRLurfVAazlDVc4aJw_f-CLG2"></div><br> --}}
                     <p class="alc mt20"><input type="submit" name="submit"></p>
                 </dl>
             </form>

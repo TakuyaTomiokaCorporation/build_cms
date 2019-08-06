983d9dea51c5ed66d0abdd77d03b67718c8db35a
冨岡　拓哉 <tomiokatakuya@tomiokatakuyanoiMac.local>
2019-08-06 17:54:52 +0900

    Edit link null able

--- database/migrations/2019_07_31_100535_add_link_to_news_products_table.php
+++ database/migrations/2019_07_31_100535_add_link_to_news_products_table.php
@@ -14,7 +14,7 @@ class AddLinkToNewsProductsTable extends Migration
     public function up()
     {
         Schema::table('news_products', function (Blueprint $table) {
-            $table->string('link');
+            $table->string('link')->nullable();
         });
     }
 

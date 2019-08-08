1566296af3d14ed36dcba0267e086920c486a13a
冨岡　拓哉 <tomiokatakuya@tomiokatakuyanoiMac.local>
2019-08-08 18:21:20 +0900

    Edit products -> continuedProducts and discontinuedProducts

--- resources/views/user/product.blade.php
+++ resources/views/user/product.blade.php
@@ -31,7 +31,7 @@
 
 @section('content')
 
-<div id="container" class="both">
+<div id="container" class="both mb60">
 <div id="" class="main" role="main">
     <article class="w1020 w98 m_auto">
     <h2 class="pt20 mb20">製品一覧</h2>
@@ -46,22 +46,44 @@
     
     <section class="product_content">
         <div class="flex">
-            @foreach ($products as $product)
+        @foreach ($continuedProducts as $continuedProduct)
             <ul class="product_box mt20 mb20">
                 <li class="alc"><a href="">
-                    <img src="{{ asset('images/thumbnails') }}/{{ $product->thumbnail }}" alt="{{ $product->product_name }}">
+                    <img src="{{ asset('images/thumbnails') }}/{{ $continuedProduct->thumbnail }}" alt="{{ $continuedProduct->product_name }}">
                 </a></li>
                 <li class="mt10 mb10"><h3>
-                    <a harf="">{{ $product->product_name }}</a>
+                    <a harf="">{{ $continuedProduct->product_name }}</a>
                 </h3></li>
                 <li>
-                    <p class="fs12">{!! Str::limit($product->overview, 100) !!}</p>
+                    <p class="fs12">{!! Str::limit($continuedProduct->overview, 100) !!}</p>
                 </li>
                 <li class="alc mt10">
-                    <a href="{{ $product->link_detail }}" class="btn_product">詳細をみる</a>
+                    <a href="{{ $continuedProduct->link_detail }}" class="btn_product">詳細をみる</a>
                 </li>
             </ul>
             @endforeach
+        </div>
+    </section>
+
+    <section class="product_content">
+        <h2 class="border_b">生産完了製品</h2>
+        <div class="flex">
+        @foreach ($discontinuedProducts as $discontinuedProduct)
+            <ul class="product_box mt20 mb20">
+                <li class="alc"><a href="">
+                    <img src="{{ asset('images/thumbnails') }}/{{ $discontinuedProduct->thumbnail }}" alt="{{ $discontinuedProduct->product_name }}">
+                </a></li>
+                <li class="mt10 mb10"><h3>
+                    <a harf="">{{ $discontinuedProduct->product_name }}</a>
+                </h3></li>
+                <li>
+                    <p class="fs12">{!! Str::limit($discontinuedProduct->overview, 100) !!}</p>
+                </li>
+                <li class="alc mt10">
+                    <a href="{{ $discontinuedProduct->link_detail }}" class="btn_product">詳細をみる</a>
+                </li>
+            </ul>
+        @endforeach
         </div>
     </section>
     

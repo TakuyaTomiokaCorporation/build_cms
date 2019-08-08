8b7e069134d205686275059fd68e04e20feffb11
冨岡　拓哉 <tomiokatakuya@tomiokatakuyanoiMac.local>
2019-08-08 15:30:12 +0900

    Edit

--- database/migrations/2019_07_08_051310_add_post_number_to_shops_table.php
+++ database/migrations/2019_07_08_051310_add_post_number_to_shops_table.php
@@ -14,11 +14,9 @@ class AddPostNumberToShopsTable extends Migration
     public function up()
     {
         Schema::table('shops', function (Blueprint $table) {
-<<<<<<< HEAD
+
             $table->string('post_number', '10');
-=======
-            $table->string('post_number', '10')->nullable();
->>>>>>> c30607e2dae65b7e0a220dda4c33e5629cfbecbb
+            
         });
     }
 

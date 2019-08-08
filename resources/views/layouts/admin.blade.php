f633ee172432c7227607054e2db8d0282bd7c6da
冨岡　拓哉 <tomiokatakuya@tomiokatakuyanoiMac.local>
2019-08-08 16:01:36 +0900

    Edit to be able to use ckeditor

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -10,7 +10,7 @@
     <title>{{ config('app.name', 'aviot') }}</title>
 
     <!-- Scripts -->
-    {{-- <script src="{{ asset('vendor/unisharp/laravel-ckeditor/ckeditor.js') }}"></script> --}}
+    <script src="{{ asset('vendor/unisharp/laravel-ckeditor/ckeditor.js') }}"></script>
     {{-- <script src="{{ asset('js/app.js') }}"></script> --}}
     
     <!-- Fonts -->

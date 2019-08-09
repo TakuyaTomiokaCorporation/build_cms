5b305c167e3650afa01d355dad0aa8a5a55e0e68
冨岡　拓哉 <tomiokatakuya@tomiokatakuyanoiMac.local>
2019-08-09 16:36:09 +0900

    Edit to go back

--- database/seeds/ShopsTableSeeder.php
+++ database/seeds/ShopsTableSeeder.php
@@ -12,8 +12,15 @@ class ShopsTableSeeder extends Seeder
      */
     public function run()
     {
-
-        $file = new SplFileObject('database/csv/shop.csv');
-        DB::table('shops') -> insert();
+        DB::table('shops') -> insert([
+            'region_id' => 1,
+            'pre_id' => 1,
+            'shop_name' => 'テックランド函館店',
+            'shop_address' => '北海道函館市亀田本町66-5',
+            'shop_tel' => '0138-44-4001',
+            'post_number' => '041-0813',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
     }
 }

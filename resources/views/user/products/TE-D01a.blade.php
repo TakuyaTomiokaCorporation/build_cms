87c2a826f42b269244239635982388bac7fe156d
冨岡　拓哉 <tomiokatakuya@tomiokatakuyanoiMac.local>
2019-08-21 12:05:24 +0900

    Create new products page

---
+++ resources/views/user/products/TE-D01a.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.user_master')
+
+@section('title', 'WE-D01b |')
+
+@section('description', '日本のサウンドを熟知した日本人オーディオエキスパートが携わる日本発のオーディオビジュアルブランド')
+
+@section('css')
+
+@endsection
+
+@section('body_id', 'product')
+
+@section('content')
+
+
+@endsection
+
+@section('javascript-footer')
+
+<script>
+    var galleryThumbs = new Swiper('.gallery-thumbs', {
+        spaceBetween: 10,
+        slidesPerView: 4,
+        freeMode: true,
+        watchSlidesVisibility: true,
+        watchSlidesProgress: true,
+    });
+    var galleryTop = new Swiper('.gallery-top', {
+        spaceBetween: 10,
+        navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+        },
+        loop: true,
+        thumbs: {
+        swiper: galleryThumbs
+        }
+    });
+</script>
+    
+@endsection

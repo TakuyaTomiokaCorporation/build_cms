43716c6dbf0c0b90d9809590a6baab3a7e9ea338
Takuya Tomioka <t-tomioka@v-trade.co.jp>
2019-08-26 14:31:45 +0900

    Create new pank_f page

---
+++ resources/views/user/artist/pnk_f.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.user_master')
+
+@section('title', 'TE-BD21f-pnk |')
+
+@section('description', '日本のサウンドを熟知した日本人オーディオエキスパートが携わる日本発のオーディオビジュアルブランド')
+
+@section('css')
+
+@endsection
+
+@section('body_id', 'product')
+
+@section('content')
+
+@endsection
+
+@section('javascript-footer')
+
+<script>
+
+</script>
+    
+@endsection

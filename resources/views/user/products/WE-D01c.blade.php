7a7b462506011a67af67e338ee742b4b153327e1
Takuya Tomioka <t-tomioka@v-trade.co.jp>
2019-08-27 16:02:53 +0900

    Edit typo

--- resources/views/user/products/WE-D01c.blade.php
+++ resources/views/user/products/WE-D01c.blade.php
@@ -19,7 +19,7 @@
         <section class="product_phot"><!--MV・商品画像-->
             <div class="swiper-container gallery-top">
                 <div class="swiper-wrapper">
-                    <div class="swiper-slide"><img src="{{ asset('images/products/WE-D01c/WE-D01_01.jpg') }}" alt=""></div>
+                    <div class="swiper-slide"><img src="{{ asset('images/products/WE-D01c/WE-D01c_01.jpg') }}" alt=""></div>
                     <div class="swiper-slide"><img src="{{ asset('images/products/WE-D01c/WE-D01c_02.png') }}" alt=""></div>
                     <div class="swiper-slide"><img src="{{ asset('images/products/WE-D01c/WE-D01c_03.png') }}" alt=""></div>
                     <div class="swiper-slide"><img src="{{ asset('images/products/WE-D01c/WE-D01c_04.png') }}" alt=""></div>
@@ -37,7 +37,7 @@
     
             <div class="swiper-container gallery-thumbs mt20 alc ifpc">
                 <div class="swiper-wrapper">
-                    <div class="swiper-slide"><img src="{{ asset('images/products/WE-D01c/WE-D01_01.jpg') }}" alt=""></div>
+                    <div class="swiper-slide"><img src="{{ asset('images/products/WE-D01c/WE-D01c_01.jpg') }}" alt=""></div>
                     <div class="swiper-slide"><img src="{{ asset('images/products/WE-D01c/WE-D01c_02.png') }}" alt=""></div>
                     <div class="swiper-slide"><img src="{{ asset('images/products/WE-D01c/WE-D01c_03.png') }}" alt=""></div>
                     <div class="swiper-slide"><img src="{{ asset('images/products/WE-D01c/WE-D01c_04.png') }}" alt=""></div>
@@ -68,7 +68,7 @@
 
     <h3 class="mt80">-Japan Tuned-</h3>
     <div class="">
-        <p class="alc"><img src="{{ asset('images/products/WE-D01c-01.jpg') }}"></p>
+        <p class="alc"><img src="{{ asset('images/products/WE-D01c/WE-D01c-01.jpg') }}"></p>
         <div class="flex_ImgR">
             <div class="flex_ImgLogo"><img src="{{ asset('images/user/JAPAN-tuned.png') }}"></div>
             <div><p class="product_text">日本の音を知り尽くしたオーディオのエキスパート達が、クラシックから最新ヒット曲まで約1,000曲の楽曲じっくりと聴き込み、徹底したチューニングを行いました。誇張の少ないフラットでナチュラルな音調に整えた上で、サウンドバランスを低域がふくよかに響くよう調整。これにより小型でありながら大編成のクラシックから、最新ヒット曲まで、ワイドレンジで、心地良く響くようになりました。</p>
@@ -78,32 +78,32 @@
     </div>
     
         <h3 class="mt80">ライブの熱を伝える新設計ハウジング</h3>
-    <p class="alc"><img src="{{ asset('images/products/WE-D01c-02.jpg') }}"></p>
+    <p class="alc"><img src="{{ asset('images/products/WE-D01c/WE-D01c-02.jpg') }}"></p>
     <div>
         <p class="product_text">AVIOTが最も得意とするTWS (完全ワイヤレスイヤホン）の設計ノウハウを活かし、重量物であるバッテリーをケーブルから排除。ハウジングケースには剛性が高く外来ノイズに強い高品位なアルミケースを採用し、音質を犠牲することなくイヤホン本体を小型化しました。フラットなトーンバランスを採用しつつ、低音量時でも豊かなベースサウンドが楽しめるようチューニングを施しました。長時間の音楽再生にも聞き疲れしません。</p>
     </div>
 
     <h3 class="mt80">クラスを超えた高音質設計</h3>
-    <p class="alc"><img src="{{ asset('images/products/WE-D01c-03.jpg') }}"></p>
+    <p class="alc"><img src="{{ asset('images/products/WE-D01c/WE-D01c-03.jpg') }}"></p>
     <div class="">
             <div><p class="product_text">ハウジング内部のアコースティックチャンバーを入念に調整し、内部共振を抑え、小型でありながら濁りの少ない中低音を実現。AVIOTが上位モデルにも採用しているハイスピード、低歪みなドライバーを使用し、中高域のリニアリティーも高めました。コーデックには一般的なSBCやAACだけでなく、より情報量の多いapt-Xにも対応しています。</p>
             </div>
     </div>
 
     <h3 class="mt80">スポーツにも最適、IPX7の防水性能</h3>
-    <p class="alc"><img src="{{ asset('images/products/WE-D01c-04.jpg') }}"></p>
+    <p class="alc"><img src="{{ asset('images/products/WE-D01c/WE-D01c-04.jpg') }}"></p>
     <div class="flex_ImgR">
             <div class="flex_ImgLogo"><img src="{{ asset('images/user/IPX7_logo.png') }}"></div>
             <div>
                 <p class="product_text">リモコンやハウジングケース部品のシーリングを強化して、IPX７レベルの防滴性能を実現しました。ジムでのハードなワークアウトやランニングでの汗や、急な雨でも平気です。</p>
-                <p  class="alc mt20"><a href="../../waterproof/" class="btn_Blk">詳細をみる</a></p>
+                <p  class="alc mt20"><a href="{{ route('waterproof') }}" class="btn_Blk">詳細をみる</a></p>
             </div>
     </div>
 
     <h3 class="mt80"></h3>
 
     <h3 class="mt80">マグネット内蔵だから、便利で安心</h3>
-    <p class="alc"><img src="{{ asset('images/products/WE-D01c-05.jpg') }}"></p>
+    <p class="alc"><img src="{{ asset('images/products/WE-D01c/WE-D01c-05.jpg') }}"></p>
     <div class="">
         <div>
             <p class="product_text">イヤホンにマグネットが内蔵されていますので、使わない時はネックレスのように首にかけられます。落とす心配もいりません。</p>
@@ -111,7 +111,7 @@
     </div>
 
     <h3 class="mt80">誰でも、簡単に操作が可能。</h3>
-    <p class="alc"><img src="{{ asset('images/products/WE-D01c-06.jpg') }}"></p>
+    <p class="alc"><img src="{{ asset('images/products/WE-D01c/WE-D01c-06.jpg') }}"></p>
         <div>
             <p class="product_text">リモコン部分で、ボリューム/再生/停止/曲送り/曲戻しなどの基本操作が可能です。またマイクが搭載されているので、ハンズフリー通話やSiri&Googleアシスタントに対応しています。</p>
         </div>
@@ -185,7 +185,7 @@
 
     <section>
         <div class="alc mt20">
-            <a href="../" class="btn_back mt10">製品一覧へ戻る</a>
+            <a href="{{ route('product.show') }}" class="btn_back mt10">製品一覧へ戻る</a>
 
             <a href="https://www.amazon.co.jp/AVIOT-WE-D01c-Bluetooth-Android-6%E6%99%82%E9%96%93%E9%80%A3%E7%B6%9A%E5%86%8D%E7%94%9F/dp/B07M7WPKF6/ref=sr_1_16?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&amp;keywords=%E3%83%AF%E3%82%A4%E3%83%A4%E3%83%AC%E3%82%B9%E3%82%A4%E3%83%A4%E3%83%9B%E3%83%B3&amp;s=gateway&amp;sr=8-16&amp;th=1" target="_blank" class="btn_buy mt10">ご購入はこちら</a>
         </div>

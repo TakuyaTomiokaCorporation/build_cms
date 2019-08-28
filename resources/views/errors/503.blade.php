ca442578bc38093e50eeec6371663fdf783ef71c
Takuya Tomioka <t-tomioka@v-trade.co.jp>
2019-08-28 20:04:24 +0900

    Edit maintanance view

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,5 +1,37 @@
-@extends('errors::minimal')
-
-@section('title', __('Service Unavailable'))
-@section('code', '503')
-@section('message', __($exception->getMessage() ?: 'Service Unavailable'))
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/reset.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/common.css') }}" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+        <h1 class="alc mt20">【現在、サイトメンテナンス中です】</h1>
+        <br>
+        <p class="alc mt20">日頃よりAVIOTをご愛顧いただきまして、誠にありがとうございます。</p><br>
+        <br>
+        <p class="alc">現在、サイトリニューアルの為、メンテナンス中となります。<br>
+        メンテナンス終了は、【2019年8月29日 17時頃】を予定しております。<br>
+        ※終了時間は、作業により多少前後することがございますこと、ご了承ください。<br>
+        </p><br>
+        <br>
+        <p class="alc">製品に関するお問い合わせ、製品登録は下記よりお願いいたします。</p><br>
+        <br>
+        <p class="alc">■製品に関するお問い合わせ<br>
+        <a href="https://v-trade.co.jp/contact_individual/">https://v-trade.co.jp/contact_individual/</a> <br>
+        </p><br>
+        <p class="alc">■製品登録<br>
+        <a href="https://v-trade.co.jp/registration/">https://v-trade.co.jp/registration/</a>  <br>
+        </p><br>
+        <p class="alc">カスタマーサポートセンター</p><br>
+        <p class="alc">電話：050-2018-3909</p><br>
+        <p class="alc">※営業時間：10:00〜17:00（土日祝除く）</p><br>
+        <br>
+        <br>
+        <p class="alc">ご不便・ご迷惑をおかけいたしますが、何卒ご理解を賜りますようお願い申し上げます。</p><br>
+        </div>        
+</body>
+</html>

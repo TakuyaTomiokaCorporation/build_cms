83ad4f3c11fc7cb46af3b5f48269ffb1f1069813
Takuya Tomioka <t-tomioka@v-trade.co.jp>
2019-08-29 13:38:43 +0900

    Create Contact mail to user

---
+++ app/Mail/ContactToUser.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class ContactToUser extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from('support@v-trade.co.jp','AVIOTサポートチーム')
+                    ->subject('【AVIOT】お問合せありがとうございます。')
+                    ->view('mails.contact_to_user');
+    }
+}

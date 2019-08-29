a80b31db5be7765ce7dc81d0ad8ba076c5982524
Takuya Tomioka <t-tomioka@v-trade.co.jp>
2019-08-29 11:16:49 +0900

    Edit email statement

--- resources/views/mails/contact.blade.php
+++ resources/views/mails/contact.blade.php
@@ -2,65 +2,65 @@
 
 <p>※本メールは自動返信です</p>
 
-<p>aviot.jpのお問合せフォームより{{ $contact_sent['$last_name'] }}様より下記の連絡が届きました。<br>
+<p>aviot.jpのお問合せフォームより{{ $contact_sent['last_name'] }}様より下記の連絡が届きました。<br>
 ご対応よろしくお願いいたします。</p>
 
 <dl>
     <dt>お名前</dt>
-    <dd>{{ $last_name.$first_name }}</dd>
+    <dd>{{ $contact_sent['last_name'].$contact_sent['first_name'] }}</dd>
 
     <dt>フリガナ</dt>
-    <dd>{{ $last_name_ja.$first_name_ja }}</dd>
+    <dd>{{ $contact_sent['last_name_ja'].$contact_sent['first_name_ja'] }}</dd>
 
     <dt>性別</dt>
-    <dd>{{ $sex }}</dd>
+    <dd>{{ $contact_sent['sex'] }}</dd>
 
     <dt>ご住所</dt>
-    <dd>{{ $zip }}</dd>
-    <dd>{{ $state }}</dd>
-    <dd>{{ $city }}</dd>
-    <dd>{{ $street }}</dd>
+    <dd>{{ $contact_sent['zip'] }}</dd>
+    <dd>{{ $contact_sent['state'] }}</dd>
+    <dd>{{ $contact_sent['city'] }}</dd>
+    <dd>{{ $contact_sent['street'] }}</dd>
     
     <dt>電話番号</dt>
-    <dd>{{ $phone }}</dd>
+    <dd>{{ $contact_sent['phone'] }}</dd>
     
     <dt>メールアドレス</dt>
-    <dd>{{ $email }}</dd>
+    <dd>{{ $contact_sent['email'] }}</dd>
 
     <dt>機種名</dt>
-    <dd>{{ $product }}</dd>
+    <dd>{{ $contact_sent['product'] }}</dd>
 
     <dt>色</dt>
     <dd>
-        @isset($color)
-        {{ $color }}
+        @isset($contact_sent['color'])
+        {{ $contact_sent['color'] }}
         @endisset
     </dd>
 
     <dt>購入日</dt>
     <dd>
-        @isset($purchase_date)
-        {{ $purchase_date }}
+        @isset($contact_sent['purchase_date'])
+        {{ $contact_sent['purchase_date'] }}
         @endisset
     </dd>
 
     <dt>購入店（miraporta）</dt>
     <dd>
-        @isset($shop_miraporta)
-        {{ $shop_miraporta }}    
+        @isset($contact_sent['shop_miraporta'])
+        {{ $contact_sent['shop_miraporta'] }}    
         @endisset
     </dd>
 
     <dt>購入店（AVIOT公式ストア/miraporta以外）</dt>
     <dd>
-        @isset($shop_other)
-        {{ $shop_other }}        
+        @isset($contact_sent['shop_other'])
+        {{ $contact_sent['shop_other'] }}        
         @endisset
     </dd>
 
     <dt>お問い合わせ分類</dt>
-    <dd>{{ $category }}</dd>
+    <dd>{{ $contact_sent['category'] }}</dd>
 
     <dt>お問い合わせ内容</dt>
-    <dd>{{ $content }}</dd>
+    <dd>{{ $contact_sent['content'] }}</dd>
 </dl>

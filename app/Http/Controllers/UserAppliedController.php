7864d280f2382a79f7363c4352eb3aa0dd47290b
Takuya Tomioka <t-tomioka@v-trade.co.jp>
2019-09-03 09:58:16 +0900

    Create Aviot Live 02 form

---
+++ app/Http/Controllers/UserAppliedController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserAppliedController extends Controller
+{
+    public function createAviotLive02Form()
+    {
+        return view('user.form.aviot_live_02');
+    }
+
+    public function confirmAviotLive02Form()
+    {
+
+    }
+
+    public function storeAviotLive02Form()
+    {
+        
+    }
+}

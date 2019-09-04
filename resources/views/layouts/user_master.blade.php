2ed47d9ac5c4d3718aee4f194702620701607980
Takuya Tomioka <t-tomioka@v-trade.co.jp>
2019-09-04 14:29:18 +0900

    Edit less footer info

--- resources/views/layouts/user_master.blade.php
+++ resources/views/layouts/user_master.blade.php
@@ -103,8 +103,8 @@
     </main>
 
     <footer>
-            <article class="w1020 w98 m_auto ifpc">
-            <div class="foot_menu">
+            <article class="w1020 w98 m_auto">
+            <div class="foot_menu ifpc">
                 <ul>
                     <li><a href="{{ route('about') }}">AVIOTについて</a></li>
                     <li><a href="{{ route('news.show') }}">最新情報</a></li>
@@ -133,6 +133,15 @@
                     <li><a href="https://www.amazon.co.jp/s?me=ABRZDPOGTSWO1" target="_blank">amazon店</a></li>
                     <li><a href="https://www.rakuten.co.jp/mobileselect/" target="_blank">楽天店</a></li>
                     <li><a href="https://store.shopping.yahoo.co.jp/mobileselect/" target="_blank">yahoo!店</a></li>
+                </ul>
+            </div>
+            <div class="foot_menu ifsp alc">
+                <ul>
+                    <li><a href="{{ route('news.show') }}">最新情報</a></li>
+                    <li><a href="{{ route('product_news.list') }}">お知らせ</a></li>
+                    <li><a href="{{ route('shoplist') }}">取扱店舗</a></li>
+                    <li><a href="{{ route('support') }}#QA">よくある質問</a></li>
+                    <li><a href="https://v-trade.co.jp/contact_individual/">お問い合わせ</a></li>
                 </ul>
             </div>
             </article>

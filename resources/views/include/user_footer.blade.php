aa883855ff8f3e13af20efb7adfa7fcb3c8d21f3
TakuyaTomioka <t-tomioka@v-trade.co.jp>
2019-09-11 16:33:32 +0900

    Create include folder and move user's header and footer

---
+++ resources/views/include/user_footer.blade.php
@@ -0,0 +1,45 @@
+<footer>
+    <article class="w1020 w98 m_auto">
+    <div class="foot_menu ifpc">
+        <ul>
+            <li><a href="{{ route('about') }}">AVIOTについて</a></li>
+            <li><a href="{{ route('news.show') }}">最新情報</a></li>
+            <li><a href="{{ route('product_news.list') }}">お知らせ</a></li>
+            <li><a href="{{ route('artists.show') }}">アーティストボイス</a></li>
+            <li><a href="{{ route('movies') }}">レビュー動画</a></li>
+        </ul>
+        <ul>
+            <li><a href="{{ route('product.show') }}">製品一覧</a></li>
+            <li><a href="{{ route('technologies') }}">技術情報</a></li>
+        </ul>
+        <ul>
+            <li><a href="{{ route('support') }}">カスタマーサポート</a></li>
+            <li><a href="{{ route('support') }}#QA">よくある質問</a></li>
+            <li><a href="https://v-trade.co.jp/contact_individual/">お問い合わせ</a></li>
+        </ul>
+        <ul>
+            <li><a href="{{ route('product_regiser') }}">製品保証</a></li>
+            <li><a href="{{ route('compensation') }}">交換保障</a></li>
+            <li><a href="{{ route('manual') }}">説明書<span class="ifpc">ダウンロード</span></a></li>
+        </ul>
+        <ul class="ifpc">
+            <li><a href="{{ route('shoplist') }}">取扱店舗</a></li>
+            <li><a href="https://www.amazon.co.jp/s?me=ABRZDPOGTSWO1" target="_blank">amazon店</a></li>
+            <li><a href="https://www.rakuten.co.jp/mobileselect/" target="_blank">楽天店</a></li>
+            <li><a href="https://store.shopping.yahoo.co.jp/mobileselect/" target="_blank">yahoo!店</a></li>
+        </ul>
+    </div>
+    <div class="foot_menu ifsp alc">
+        <ul>
+            <li><a href="{{ route('news.show') }}">最新情報</a></li>
+            <li><a href="{{ route('product_news.list') }}">お知らせ</a></li>
+            <li><a href="{{ route('shoplist') }}">取扱店舗</a></li>
+            <li><a href="{{ route('support') }}#QA">よくある質問</a></li>
+            <li><a href="https://v-trade.co.jp/contact_individual/">お問い合わせ</a></li>
+        </ul>
+    </div>
+    </article>
+    <div id="copyright">
+        <p>&copy;2019 AIOT</p>
+    </div>
+</footer>

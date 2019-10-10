eb998a1c2d457a5132dd15ccaab849cfa93fcf91
TakuyaTomioka <t-tomioka@v-trade.co.jp>
2019-10-10 16:10:30 +0900

    Edit button

--- resources/views/user/interview/nomura.blade.php
+++ resources/views/user/interview/nomura.blade.php
@@ -21,15 +21,16 @@
     <h2 class="pt20 mb20">VGP審査員　野村ケンジが語る</h2>
 
     <section>
-            <div class=""><img src="{{ asset('images/interview/nomura.jpg') }}" class=""></div>
-            <div class="mt20">
-                <h3 id="tebd21f">TE-BD21f</h3>
-                <h4 class="mt10">新時代のレファレンスモデル</h4>
-                <p class="mt10">完全ワイヤレスイヤホンは機能性と共にコンパクトさも追求しなければならない製品のため、音質的に不利になる傾向があるのは確かです。</p>
-                <p class="mt10">しかしながらTE-BD21fは、完全ワイヤレスであることを言い訳にせず、ハイグレードモデルと呼ぶにふさわしい良音質と表現力の豊かさを実現しています。</p>
-                <p class="mt10">音質に関しては、完全ワイヤレスイヤホンの新しいレファレンスとなる存在です。ハイブリッド・トリプルドライバーの採用など、豪華な仕様の割にリーズナブルな価格設定も嬉しいかぎりです。</p>
-            </div>
-        <p class="alc mt20"><a href="{{ route('tebd21f') }}" class="btn_Blk">製品詳細はこちら</a></p>
+        <div class=""><img src="{{ asset('images/interview/nomura.jpg') }}" class=""></div>
+        <div class="mt20">
+            <h3 id="tebd21f">TE-BD21f</h3>
+            <h4 class="mt10">新時代のレファレンスモデル</h4>
+            <p class="mt10">完全ワイヤレスイヤホンは機能性と共にコンパクトさも追求しなければならない製品のため、音質的に不利になる傾向があるのは確かです。</p>
+            <p class="mt10">しかしながらTE-BD21fは、完全ワイヤレスであることを言い訳にせず、ハイグレードモデルと呼ぶにふさわしい良音質と表現力の豊かさを実現しています。</p>
+            <p class="mt10">音質に関しては、完全ワイヤレスイヤホンの新しいレファレンスとなる存在です。ハイブリッド・トリプルドライバーの採用など、豪華な仕様の割にリーズナブルな価格設定も嬉しいかぎりです。</p>
+        </div>
+        <p class="alc mt20"><a href="{{ route('tebd21f') }}" class="btn_buy">製品詳細はこちら</a></p>
+        <p class="alc mt20"><a href="{{ route('interview.pro') }}" class="btn_back">一覧に戻る</a></p>
     </section>
 
 
--- resources/views/user/interview/orihara.blade.php
+++ resources/views/user/interview/orihara.blade.php
@@ -21,15 +21,16 @@
     <h2 class="pt20 mb20">ライフスタイル部会副座長　折原一也が語る</h2>
 
     <section>
-            <div class=""><img src="{{ asset('images/interview/orihara.jpg') }}" class=""></div>
-            <div class="mt20">
-                <h3 id="tebd21f">TE-BD21f</h3>
-                <h4 class="mt10">注目度トップの完全ワイヤレス</h4>
-                <p class="mt10">完全ワイヤレスイヤホンに、ダイナミックドライバーとBAドライバーを併せたハイブリッド・トリプルドライバーを搭載した、ポータブル・オーディオファンにとって注目度トップのモデルと言っても過言ではありません。</p>
-                <p class="mt10">実際にそのサウンドを聴いてみると、極めてピュアな傾向で音の情報量も完全ワイヤレスイヤホンと思えないほど。</p>
-                <p class="mt10">本格的なサウンドを完全ワイヤレスイヤホンで味わいたい方に、真っ先にお薦めしたいモデルです。</p>
-            </div>
-        <p class="alc mt20"><a href="{{ route('tebd21f') }}" class="btn_Blk">製品詳細はこちら</a></p>
+        <div class=""><img src="{{ asset('images/interview/orihara.jpg') }}" class=""></div>
+        <div class="mt20">
+            <h3 id="tebd21f">TE-BD21f</h3>
+            <h4 class="mt10">注目度トップの完全ワイヤレス</h4>
+            <p class="mt10">完全ワイヤレスイヤホンに、ダイナミックドライバーとBAドライバーを併せたハイブリッド・トリプルドライバーを搭載した、ポータブル・オーディオファンにとって注目度トップのモデルと言っても過言ではありません。</p>
+            <p class="mt10">実際にそのサウンドを聴いてみると、極めてピュアな傾向で音の情報量も完全ワイヤレスイヤホンと思えないほど。</p>
+            <p class="mt10">本格的なサウンドを完全ワイヤレスイヤホンで味わいたい方に、真っ先にお薦めしたいモデルです。</p>
+        </div>
+        <p class="alc mt20"><a href="{{ route('tebd21f') }}" class="btn_buy">製品詳細はこちら</a></p>
+        <p class="alc mt20"><a href="{{ route('interview.pro') }}" class="btn_back">一覧に戻る</a></p>
     </section>
 
 

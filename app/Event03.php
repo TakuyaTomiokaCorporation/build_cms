351a455e1d001faf068f83a5b13bee003670e1dd
TakuyaTomioka <t-tomioka@v-trade.co.jp>
2019-11-13 11:45:41 +0900

    Create model for event3

---
+++ app/Event03.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Event03 extends Model
+{
+    protected $table = 'event3';
+
+    protected $casts = [
+        'age' => 'integer'
+    ];
+
+    protected $fillable = [
+        'email',
+        'sex',
+        'age',
+        'recognition_from',
+        'musician',
+        'smart_phone',
+        'exception'
+    ];
+    
+}
